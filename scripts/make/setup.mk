7b9d0d01ca0b6027202e7bcecf9b2a6890c7c1f2
Vandcarlos Mouzinho Sandes Junior <vandcarlosjunior@gmail.com>
2026-04-28 23:12:52 -0300

    Ajustado setup.mk para CI

--- scripts/make/setup.mk
+++ scripts/make/setup.mk
@@ -1,17 +1,17 @@
 .PHONY: setup-notebooks setup-ingest setup-tests  setup
 
-setup-notebooks: venv ## Instala as dependências necessárias para rodar os notebooks
+setup-notebooks: $(VENV_DEP) ## Instala as dependências necessárias para rodar os notebooks
 	@echo "Installing notebook dependencies..."; \
 	$(PIP) install -e ".[notebooks]"
 
-setup-ingest: venv ## Instala as dependências necessárias para rodar os scripts de ingestão
+setup-ingest: $(VENV_DEP) ## Instala as dependências necessárias para rodar os scripts de ingestão
 	@echo "Installing ingest dependencies"; \
 	$(PIP) install -e ".[ingest]"
 
-setup-tests: venv ## Instala as dependências necessárias para rodar os testes
+setup-tests: $(VENV_DEP) ## Instala as dependências necessárias para rodar os testes
 	@echo "Installing test dependencies..."; \
 	$(PIP) install -e ".[tests,ingest]"
 
-setup: venv ## Instala todas as dependências do projeto
+setup: $(VENV_DEP) ## Instala todas as dependências do projeto
 	@echo "Installing project dependencies..."; \
 	$(PIP) install -e .[dev,tests,ingest,notebooks]
