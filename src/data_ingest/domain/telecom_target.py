c9980e4f4ea79961e058e7ef42339f7691569e60
Vandcarlos Mouzinho Sandes Junior <vandcarlosjunior@gmail.com>
2026-04-28 23:34:36 -0300

    Ajustado versão do Python no action.yml

--- src/data_ingest/domain/telecom_target.py
+++ src/data_ingest/domain/telecom_target.py
@@ -1,6 +1,8 @@
 import pandera.pyspark as pa
 from pyspark.sql.types import DoubleType, IntegerType, StringType
 
+from . import telecom_source
+
 
 class TelecomTarget(pa.DataFrameModel):
     """
@@ -9,36 +11,29 @@ class TelecomTarget(pa.DataFrameModel):
     """
 
     # Identificação
-    gender: StringType = pa.Field(isin=["Female", "Male"])
-    senior_citizen: StringType = pa.Field(isin=["No", "Yes"])
-    partner: StringType = pa.Field(isin=["Yes", "No"])
-    dependents: StringType = pa.Field(isin=["Yes", "No"])
+    gender: StringType = pa.Field(isin=telecom_source.GENDER_OPTIONS)
+    senior_citizen: StringType = pa.Field(isin=telecom_source.SENIOR_CITIZEN_OPTIONS)
+    partner: StringType = pa.Field(isin=telecom_source.PARTNER_OPTIONS)
+    dependents: StringType = pa.Field(isin=telecom_source.DEPENDENTS_OPTIONS)
 
     # Métricas de tempo
     tenure_months: IntegerType = pa.Field(ge=0)
 
     # Serviços (padronizados)
-    phone_service: StringType = pa.Field(isin=["Yes", "No"])
-    multiple_lines: StringType = pa.Field(isin=["No", "Yes"])
-    internet_service: StringType = pa.Field(isin=["No", "DSL", "Fiber optic", "Cable"])
-    online_security: StringType = pa.Field(isin=["Yes", "No"])
-    online_backup: StringType = pa.Field(isin=["Yes", "No"])
-    device_protection: StringType = pa.Field(isin=["Yes", "No"])
-    tech_support: StringType = pa.Field(isin=["Yes", "No"])
-    streaming_tv: StringType = pa.Field(isin=["Yes", "No"])
-    streaming_movies: StringType = pa.Field(isin=["Yes", "No"])
+    phone_service: StringType = pa.Field(isin=telecom_source.PHONE_SERVICE_OPTIONS)
+    multiple_lines: StringType = pa.Field(isin=telecom_source.MULTIPLE_LINES_OPTIONS)
+    internet_service: StringType = pa.Field(isin=telecom_source.INTERNET_SERVICE_OPTIONS)
+    online_security: StringType = pa.Field(isin=telecom_source.ONLINE_SECURITY_OPTIONS)
+    online_backup: StringType = pa.Field(isin=telecom_source.ONLINE_BACKUP_OPTIONS)
+    device_protection: StringType = pa.Field(isin=telecom_source.DEVICE_PROTECTION_OPTIONS)
+    tech_support: StringType = pa.Field(isin=telecom_source.TECH_SUPPORT_OPTIONS)
+    streaming_tv: StringType = pa.Field(isin=telecom_source.STREAMING_TV_OPTIONS)
+    streaming_movies: StringType = pa.Field(isin=telecom_source.STREAMING_MOVIES_OPTIONS)
 
     # Faturamento
-    contract: StringType = pa.Field(isin=["Month-to-month", "One year", "Two year"])
-    paperless_billing: StringType = pa.Field(isin=["Yes", "No"])
-    payment_method: StringType = pa.Field(
-        isin=[
-            "Electronic check",
-            "Mailed check",
-            "Bank transfer (automatic)",
-            "Credit card (automatic)",
-        ]
-    )
+    contract: StringType = pa.Field(isin=telecom_source.CONTRACT_OPTIONS)
+    paperless_billing: StringType = pa.Field(isin=telecom_source.PAPERLESS_BILLING_OPTIONS)
+    payment_method: StringType = pa.Field(isin=telecom_source.PAYMENT_METHOD_OPTIONS)
     monthly_charges: DoubleType = pa.Field(ge=0)
     total_charges: DoubleType = pa.Field(nullable=True, ge=0)
 
