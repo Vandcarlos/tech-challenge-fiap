f54910104c0d0690980eac82b9793c7f2719a437
Vand <vandcarlosjunior@gmail.com>
2026-04-30 00:06:48 -0300

    [WIP] Criando transfomer

--- src/data_ingest/domain/__init__.py
+++
@@ -1,4 +0,0 @@
-from .telecom_source import TelecomSource
-from .telecom_target import TelecomTarget
-
-__all__ = ["TelecomSource", "TelecomTarget"]
---
+++ src/services/train/transformer.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+from scipy.sparse import spmatrix
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from .dataset import Dataset
+
+
+class Transformer:
+    X_train: np.ndarray | spmatrix
+    y_train: pd.Series
+    X_val: np.ndarray | spmatrix
+    y_val: pd.Series
+    X_test: np.ndarray | spmatrix
+    y_test: pd.Series
+    preprocessor: ColumnTransformer
+
+    def __init__(self, dataset: Dataset):
+        preprocessor = _build_preprocessor()
+
+        X_train_processed = preprocessor.fit_transform(dataset.X_train)
+        X_val_processed = preprocessor.transform(dataset.X_val)
+        X_test_processed = preprocessor.transform(dataset.X_test)
+
+        self.X_train = X_train_processed
+        self.y_train = dataset.y_train
+        self.X_val = X_val_processed
+        self.y_val = dataset.y_val
+        self.X_test = X_test_processed
+        self.y_test = dataset.y_test
+        self.preprocessor = preprocessor
+
+
+def _build_preprocessor():
+    categorical_features = [
+        "gender",
+        "senior_citizen",
+        "partner",
+        "dependents",
+        "phone_service",
+        "multiple_lines",
+        "internet_service",
+        "online_security",
+        "online_backup",
+        "device_protection",
+        "tech_support",
+        "streaming_tv",
+        "streaming_movies",
+        "contract",
+        "paperless_billing",
+        "payment_method",
+    ]
+    numeric_features = ["tenure_months", "monthly_charges", "total_charges"]
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", StandardScaler(), numeric_features),
+            ("cat", OneHotEncoder(drop="first", handle_unknown="ignore"), categorical_features),
+        ]
+    )
+
+    return preprocessor
