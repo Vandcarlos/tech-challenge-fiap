81582395bed949cd1506cb3725ac456a8967af97
Vand <vandcarlosjunior@gmail.com>
2026-05-04 19:01:27 -0300

    Adicionado eval no treino

--- src/services/train/tensors.py
+++ src/services/train/tensors.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 import torch
 
-from .transformer import Transformer
+from .preprocessor import Preprocessor
 
 
 @dataclass
@@ -14,12 +14,12 @@ class Tensors:
     X_test: torch.Tensor
     y_test: torch.Tensor
 
-    def __init__(self, transformer: Transformer):
-        self.X_train = torch.tensor(transformer.X_train, dtype=torch.float32)
-        self.y_train = torch.tensor(transformer.y_train.values, dtype=torch.float32).view(-1, 1)
+    def __init__(self, preprocessor: Preprocessor):
+        self.X_train = torch.tensor(preprocessor.X_train, dtype=torch.float32)
+        self.y_train = torch.tensor(preprocessor.y_train.values, dtype=torch.float32).view(-1, 1)
 
-        self.X_val = torch.tensor(transformer.X_val, dtype=torch.float32)
-        self.y_val = torch.tensor(transformer.y_val.values, dtype=torch.float32).view(-1, 1)
+        self.X_val = torch.tensor(preprocessor.X_val, dtype=torch.float32)
+        self.y_val = torch.tensor(preprocessor.y_val.values, dtype=torch.float32).view(-1, 1)
 
-        self.X_test = torch.tensor(transformer.X_test, dtype=torch.float32)
-        self.y_test = torch.tensor(transformer.y_test.values, dtype=torch.float32).view(-1, 1)
+        self.X_test = torch.tensor(preprocessor.X_test, dtype=torch.float32)
+        self.y_test = torch.tensor(preprocessor.y_test.values, dtype=torch.float32).view(-1, 1)
